bbbf266302ddb5ce51074e62dd4af4ab9c8b6bcd
lzy <2372459615@qq.com>
2021-05-11 09:47:49 +0800

    first-commit

---
+++ src/main/java/com/lzy/git/GItTest.java
@@ -0,0 +1,17 @@
+package com.lzy.git;
+
+/**
+ * @author
+ * @version 1.0
+ * @description
+ * @updateRemark
+ * @updateUser
+ * @createDate 2021/5/11 9:45
+ * @updateDate 2021/5/11 9:45
+ **/
+
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
